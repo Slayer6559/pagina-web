12c4c41a069e1d936c41157dcc7e7f2ba76db33f
Slayer6559 <137456954+Slayer6559@users.noreply.github.com>
2023-06-25 19:43:25 -0300

    Add files via upload

---
+++ estilo html.js
@@ -0,0 +1,8 @@
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function () {
+        document.querySelector(".nav-links").
+            classList.toggle("nav-links-responsive")
+    })
+
+function showAlert() { alert('Tu formulario ha sido enviado con exito, gracias!');
+}
